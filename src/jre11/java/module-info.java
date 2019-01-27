62e548d461aca60938e20bbfc15831fe79673ec8
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:25 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,17 +1,11 @@
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.guicedpersistence.readers.systemproperties.SystemEnvironmentModuleExclusions;
-import com.jwebmp.guicedpersistence.readers.systemproperties.SystemEnvironmentVariablesPropertiesReader;
-import com.jwebmp.guicedpersistence.services.IPropertiesEntityManagerReader;
-
 module com.jwebmp.guicedpersistence.readers.systemproperties {
 	requires com.jwebmp.guicedpersistence;
 	requires com.google.common;
 	requires com.jwebmp.guicedinjection;
 	requires java.validation;
 
-	provides IPropertiesEntityManagerReader with SystemEnvironmentVariablesPropertiesReader;
-	provides IGuiceScanModuleExclusions with SystemEnvironmentModuleExclusions;
-	provides IGuiceScanJarExclusions with SystemEnvironmentModuleExclusions;
+	provides com.jwebmp.guicedpersistence.services.IPropertiesEntityManagerReader with com.jwebmp.guicedpersistence.readers.systemproperties.SystemEnvironmentVariablesPropertiesReader;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.guicedpersistence.readers.systemproperties.SystemEnvironmentModuleExclusions;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.guicedpersistence.readers.systemproperties.SystemEnvironmentModuleExclusions;
 
 }
