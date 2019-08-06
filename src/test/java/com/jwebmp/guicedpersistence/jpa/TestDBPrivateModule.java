d470a3946ef03e8ca028422521e1bd3ab6087bf2
GedMarc <ged_marc@hotmail.com>
2019-08-06 20:00:21 +0200

    JDK 12 Build and Various

--- src/test/java/com/jwebmp/guicedpersistence/jpa/TestDBPrivateModule.java
+++ src/test/java/com/jwebmp/guicedpersistence/jpa/TestDBPrivateModule.java
@@ -4,6 +4,7 @@ import com.jwebmp.guicedpersistence.db.DatabaseModule;
 import com.jwebmp.guicedpersistence.db.ConnectionBaseInfo;
 import com.oracle.jaxb21.PersistenceUnit;
 
+import javax.sql.DataSource;
 import javax.validation.constraints.NotNull;
 import java.lang.annotation.Annotation;
 import java.util.Properties;
@@ -22,7 +23,13 @@ public class TestDBPrivateModule
 	@Override
 	protected @NotNull ConnectionBaseInfo getConnectionBaseInfo(PersistenceUnit unit, Properties filteredProperties)
 	{
-		return new JPAConnectionBaseInfo().setPersistenceUnitName(unit.getName());
+		return new ConnectionBaseInfo() {
+			@Override
+			public DataSource toPooledDatasource()
+			{
+				return null;
+			}
+		}.setPersistenceUnitName(unit.getName());
 	}
 
 	@NotNull
