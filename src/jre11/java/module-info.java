6b35cc3ef1314e14da40c7b011ec7e40f28c4049
GedMarc <ged_marc@hotmial.com>
2020-03-02 10:28:11 +0200

    Module Linting

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -5,7 +5,5 @@ module com.guicedee.guicedpersistence.readers.systemproperties {
 	requires java.validation;
 
 	provides com.guicedee.guicedpersistence.services.IPropertiesEntityManagerReader with com.guicedee.guicedpersistence.readers.systemproperties.SystemEnvironmentVariablesPropertiesReader;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.guicedee.guicedpersistence.readers.systemproperties.SystemEnvironmentModuleExclusions;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.guicedee.guicedpersistence.readers.systemproperties.SystemEnvironmentModuleExclusions;
 
 }
