1688e6601c9e7ebcf9b1dd0a2c9b8477c24a23cd
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:21 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/main/java/com/guicedee/guicedpersistence/readers/systemproperties/SystemEnvironmentModuleExclusions.java
+++ src/main/java/com/guicedee/guicedpersistence/readers/systemproperties/SystemEnvironmentModuleExclusions.java
@@ -1,6 +1,5 @@
 package com.guicedee.guicedpersistence.readers.systemproperties;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,17 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SystemEnvironmentModuleExclusions
-		implements IGuiceScanModuleExclusions<SystemEnvironmentModuleExclusions>,
-				           IGuiceScanJarExclusions<SystemEnvironmentModuleExclusions>
+		implements IGuiceScanModuleExclusions<SystemEnvironmentModuleExclusions>
 {
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("guiced-persistence-systemproperties-reader-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
